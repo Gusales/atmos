45eacbc7f072ba339ffd53d0876b83b1dec17a6d
Gustavo Sales <dev.gussales@gmail.com>
2026-09-20 21:53:34 -0300

    refactor: replace inline icon resolution in CurrentWeatherComponent with WeatherIconComponent
    - Remove the duplicated icon path/alt lookup logic (alternativeTexts, baseIconPath, baseIconAlt, resolveWeatherNames) from CurrentWeatherComponent
    - Render the current weather icon via the shared WeatherIconComponent, passing currentWeather directly

--- app/src/app/components/current-weather/index.ts
+++ app/src/app/components/current-weather/index.ts
@@ -1,60 +1,21 @@
 import { Component, Input } from "@angular/core";
+import { WeatherIconComponent } from "../../shared/components/ui/icons/weather-icon";
 import { WeatherNamesEnum } from "../../shared/enums";
-
-
-const alternativeTexts = {
-    clouds: 'Ícone de tempo nublado',
-    sun: 'Ícone de tempo ensolarado',
-    'clouds-rain': 'Ícone de tempo chuvoso',
-    'clouds-thunder': 'Ícone de tempo tempestuoso',
-    'clouds-sun': 'Ícone de tempo parcialmente nublado, com sol e nuvens'
-} as const
-
-const baseIconPath = (iconName: keyof typeof alternativeTexts) => `/assets/svg/${iconName}.svg`
-
-const baseIconAlt = (iconName: keyof typeof alternativeTexts) => {
-    return alternativeTexts[iconName]
-}
 
 @Component({
     selector: 'app-current-weather',
     templateUrl: './current-weather.component.html',
-    standalone: true
+    standalone: true,
+    imports: [WeatherIconComponent]
 })
 export class CurrentWeatherComponent {
-    @Input() protected readonly currentWeather: WeatherNamesEnum = WeatherNamesEnum.SUN
+    @Input() currentWeather: WeatherNamesEnum = WeatherNamesEnum.SUN
 
-    @Input() protected readonly city: string = "São Paulo, SP"
-    @Input() protected readonly currentTemperature: number = 0
-    @Input() protected readonly minimumTemperature: number = 0
-    @Input() protected readonly maximunTemperature: number = 0
-    @Input() protected readonly windSpeed: number = 0
-    @Input() protected readonly humidity: number = 0
-    @Input() protected readonly rainProbabilityPercent: number = 0
-
-    protected get getCurrentWeatherIcon() {
-        return baseIconPath(this.resolveWeatherNames(this.currentWeather))
-    }
-
-    protected get getCurrentWeatherIconAlt() {
-        return baseIconAlt(this.resolveWeatherNames(this.currentWeather))
-    }
-
-    private resolveWeatherNames(currentWeather: WeatherNamesEnum): keyof typeof alternativeTexts {
-        switch (currentWeather) {
-            case WeatherNamesEnum.SUN:
-                return 'sun'
-            case WeatherNamesEnum.CLOUDY:
-                return 'clouds'
-            case WeatherNamesEnum.PARTLY_CLOUDY:
-                return 'clouds-sun'
-            case WeatherNamesEnum.RAIN:
-                return 'clouds-sun'
-            case WeatherNamesEnum.THUNDER:
-                return 'clouds-thunder'
-            default:
-                return 'sun'
-        }
-    }
-
+    @Input() city: string = "São Paulo, SP"
+    @Input() currentTemperature: number = 0
+    @Input() minimumTemperature: number = 0
+    @Input() maximunTemperature: number = 0
+    @Input() windSpeed: number = 0
+    @Input() humidity: number = 0
+    @Input() rainProbabilityPercent: number = 0
 }
